f68e167193471bd603bec17bb4d83e6feaada674
yonassolomon <yonaisol21@gmail.com>
2026-03-17 16:20:47 -1000

    Add base templates and configurations

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    context = {
+        'name': 'YONAS SOLOMON',
+        'title': 'Software Engineering Student',
+    }
+    return render(request, 'portfolio/home.html', context)
